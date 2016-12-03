8c501169631b0ecf2137bc6abef0f7a6d4107a3d
pavel081215 <pavelzag12@gmail.com>
2016-12-03 16:29:42 +0200

    type of the future + actor

--- src/main/scala/com/zagrebelnyy/spray/server/Main.scala
+++ src/main/scala/com/zagrebelnyy/spray/server/Main.scala
@@ -1,33 +1,48 @@
 package com.zagrebelnyy.spray.server
 
 import akka.actor.{Props, Actor, ActorSystem}
+import akka.util.Timeout
 import com.zagrebelnyy.spray._
 import spray.http.MediaTypes
-
+import akka.pattern.ask
+import scala.concurrent.duration._
 import spray.routing.{RequestContext, Route, SimpleRoutingApp}
 
 
 object Main extends App with SimpleRoutingApp {
-
   implicit val actorSystem = ActorSystem()
 
+  import actorSystem.dispatcher
+
+  implicit val timeout = Timeout(1.second)
 
   var plentyOfDrugs = Drug.drugs
 
 
   lazy val helloActor = actorSystem.actorOf(Props(new FirstActor))
+  lazy val burnActor = actorSystem.actorOf(Props(new BurnActor))
 
-  lazy val helloroute = {
+  lazy val helloroute = get {
     path("hello") {
       complete {
         "Welcome to my main)))))))))))))))))"
       }
     }
   }
-  lazy val helloroute2 = {
+  lazy val helloroute2 = get {
     path("hello2") {
 
       ctx => helloActor ! ctx
+    }
+  }
+
+  lazy val burnRoute = get {
+    path("burn" / "remaining") {
+      complete {
+        (burnActor ? RemainingBurningTime)
+          .mapTo[Int]
+          .map(s => s"The remaining burning time is $s")
+      }
     }
   }
 
@@ -42,8 +57,9 @@ object Main extends App with SimpleRoutingApp {
 
 
   startServer(interface = "Localhost", port = 8080) {
-    helloroute ~
+      helloroute ~
       helloroute2 ~
+      burnRoute ~
       getJson {
         path("list" / "all") {
           complete {
@@ -85,10 +101,17 @@ object Main extends App with SimpleRoutingApp {
   class FirstActor extends Actor {
     override def receive = {
       case ctx: RequestContext => ctx.complete("First Actor!!!!!!!!!!!!!!!!!!!!! SUPER")
-
     }
-
   }
 
+  class BurnActor extends Actor {
+    val remainingTime = 10
+
+    override def receive = {
+      case RemainingBurningTime => sender ! remainingTime
+    }
+  }
+
+  object RemainingBurningTime
 
 }

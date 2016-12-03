8289f9d67cbdf0cd1f2e7d84313ab845f0490b58
pavel081215 <pavelzag12@gmail.com>
2016-12-03 13:15:16 +0200

    actor

--- src/main/scala/com/zagrebelnyy/spray/server/Main.scala
+++ src/main/scala/com/zagrebelnyy/spray/server/Main.scala
@@ -1,15 +1,21 @@
 package com.zagrebelnyy.spray.server
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, Actor, ActorSystem}
 import com.zagrebelnyy.spray._
 import spray.http.MediaTypes
 
-import spray.routing.{Route, SimpleRoutingApp}
+import spray.routing.{RequestContext, Route, SimpleRoutingApp}
 
 
 object Main extends App with SimpleRoutingApp {
+
   implicit val actorSystem = ActorSystem()
+
+
   var plentyOfDrugs = Drug.drugs
+
+
+  lazy val helloActor = actorSystem.actorOf(Props(new FirstActor))
 
   lazy val helloroute = {
     path("hello") {
@@ -18,6 +24,13 @@ object Main extends App with SimpleRoutingApp {
       }
     }
   }
+  lazy val helloroute2 = {
+    path("hello2") {
+
+      ctx => helloActor ! ctx
+    }
+  }
+
 
   def getJson(route: Route): Route = {
     get {
@@ -27,9 +40,10 @@ object Main extends App with SimpleRoutingApp {
     }
   }
 
-  //startServer(interface = "Localhost", port = 8080) {
+
   startServer(interface = "Localhost", port = 8080) {
     helloroute ~
+      helloroute2 ~
       getJson {
         path("list" / "all") {
           complete {
@@ -66,6 +80,14 @@ object Main extends App with SimpleRoutingApp {
           }
         }
       }
+  }
+
+  class FirstActor extends Actor {
+    override def receive = {
+      case ctx: RequestContext => ctx.complete("First Actor!!!!!!!!!!!!!!!!!!!!! SUPER")
+
+    }
+
   }
 
 
